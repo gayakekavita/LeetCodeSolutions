b29cffe8876e5179948ad6332d14f1cfc6e9fc5b
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-10-11 22:30:03 +0530

    Create count_sorted_vowel_strings.java

---
+++ count_sorted_vowel_strings.java
@@ -0,0 +1,18 @@
+class count_sorted_vowel_strings {
+    
+    public  int countVowelStrings(int n) {
+       int a=1,e=1,i=1,o=1,u=1;
+       while(n>1)
+       {
+           a=(a+e+i+o+u);
+           e=(e+i+o+u);
+           i=(i+o+u);
+           o=(o+u);
+           u=(u);
+           n--;
+       }
+       return a+e+i+o+u;
+
+    }
+    
+}
